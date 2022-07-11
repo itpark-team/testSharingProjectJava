223393c78700ea82e251ff09e9dcc2744f8844a3
itpark <a.m.smyslov@yandex.ru>
2022-07-11 20:25:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int result = a + b;
+        System.out.println("result = " + result);
+    }
+}
